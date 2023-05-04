636fd8e5c05aac69f27709f01bc31b67452bba05
Marcos Duarte <marcosduarte1994@gmail.com>
2023-05-04 17:24:26 -0300

    chore: changes in file

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,72 +1,103 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Predicate;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Properties;
+public class FileProcessor {
+    private static final String INPUT_TOPIC = "input_files";
+    private static final String OUTPUT_TOPIC = "processed_files";
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-public class DocumentProcessor {
-    public static void main(String[] args) {
-        // Configurações do Kafka Streams
+    public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("application.id", "document-processor");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "file-processor");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 
-        // Cria um StreamsBuilder
         StreamsBuilder builder = new StreamsBuilder();
 
-        // Define o tópico de entrada
-        KStream<String, byte[]> documents = builder.stream("input-topic");
+        KStream<String, String> inputStream = builder.stream(INPUT_TOPIC);
 
-        // Filtra apenas os arquivos com extensão .xlsx e processa-os
-        KStream<String, byte[]> excelDocuments = documents.filter((key, value) -> key.endsWith(".xlsx"));
-        excelDocuments.foreach((key, value) -> {
-            // Processa o arquivo Excel
-            // ...
+        KStream<String, String> outputStream = inputStream
+                .mapValues(value -> {
+                    String fileExtension = getFileExtension(value);
+                    switch (fileExtension) {
+                        case "xlsx":
+                            return processExcelFile(value);
+                        case "txt":
+                            return processTxtFile(value);
+                        case "cnab240":
+                            return processCnab240File(value);
+                        case "cnab400":
+                            return processCnab400File(value);
+                        default:
+                            return "Unsupported file type";
+                    }
+                });
 
-            // Envia o resultado para o tópico de saída
-            // ...
-        });
+        outputStream.to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
 
-        // Filtra apenas os arquivos com extensão .txt e processa-os
-        KStream<String, byte[]> txtDocuments = documents.filter((key, value) -> key.endsWith(".txt"));
-        txtDocuments.foreach((key, value) -> {
-            // Processa o arquivo TXT
-            // ...
-
-            // Envia o resultado para o tópico de saída
-            // ...
-        });
-
-        // Filtra apenas os arquivos com extensão .cnab240 e processa-os
-        KStream<String, byte[]> cnab240Documents = documents.filter((key, value) -> key.endsWith(".cnab240"));
-        cnab240Documents.foreach((key, value) -> {
-            // Processa o arquivo CNAB240
-            // ...
-
-            // Envia o resultado para o tópico de saída
-            // ...
-        });
-
-        // Filtra apenas os arquivos com extensão .cnab400 e processa-os
-        KStream<String, byte[]> cnab400Documents = documents.filter((key, value) -> key.endsWith(".cnab400"));
-        cnab400Documents.foreach((key, value) -> {
-            // Processa o arquivo CNAB400
-            // ...
-
-            // Envia o resultado para o tópico de saída
-            // ...
-        });
-
-        // Define o tópico de saída para cada tipo de documento processado
-        excelDocuments.to("output-excel-topic", Produced.with(org.apache.kafka.common.serialization.Serdes.String(), org.apache.kafka.common.serialization.Serdes.ByteArray()));
-        txtDocuments.to("output-txt-topic", Produced.with(org.apache.kafka.common.serialization.Serdes.String(), org.apache.kafka.common.serialization.Serdes.ByteArray()));
-        cnab240Documents.to("output-cnab240-topic", Produced.with(org.apache.kafka.common.serialization.Serdes.String(), org.apache.kafka.common.serialization.Serdes.ByteArray()));
-        cnab400Documents.to("output-cnab400-topic", Produced.with(org.apache.kafka.common.serialization.Serdes.String(), org.apache.kafka.common.serialization.Serdes.ByteArray()));
-
-        // Cria o KafkaStreams e inicia o processamento
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println("Processed files: " + outputStream.count().get());
+        }
+    }
+
+    private static String processExcelFile(String filePath) {
+        try {
+            Workbook workbook = new XSSFWorkbook(new File(filePath));
+            Sheet sheet = workbook.getSheetAt(0);
+            StringBuilder output = new StringBuilder();
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    output.append(cell.toString());
+                }
+            }
+            return output.toString();
+        } catch (IOException e) {
+            return "Error processing Excel file: " + e.getMessage();
+        }
+    }
+
+    private static String processTxtFile(String filePath) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            return new String(bytes);
+        } catch (IOException e) {
+            return "Error processing TXT file: " + e.getMessage();
+        }
+    }
+
+    private static String processCnab240File(String filePath) {
+        // TODO: implement CNAB240 processing logic
+        return "CNAB240 file processing not implemented yet";
+    }
+
+    private static String processCnab400File(String filePath) {
+        // TODO: implement CNAB400 processing logic
+        return "CNAB400 file processing not implemented yet";
+    }
+
+    private static String getFileExtension(String filePath) {
+        int lastIndex = filePath.lastIndexOf('.');
+        if (lastIndex == -1) {
+            return "";
+        } else {
+            return filePath.substring(lastIndex + 1);
+        }
     }
 }
